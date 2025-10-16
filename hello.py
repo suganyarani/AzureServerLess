46763395e1d10bc242a576d369763ec14685b6da
Suganya <572490@cognizant.com>
2025-10-16 21:04:33 +0530

    add flag to control python test

--- hello.py
+++ hello.py
@@ -1,16 +1,16 @@
 import sys
 
-def main(arg):
-    print(arg)
-    with open("testdata/genai-testdata.jsonl", "r") as infile, open(arg, "w") as outfile:
-        for line in infile:
-            outfile.write(line)
-    print("success")
-
-if __name__ == "__main__":
-    print("HELLO WORLD")
-    print("Starting script...")
+def main(arg):   
+    try: 
+        with open("testdata/genai-testdata.jsonl", "r") as infile, open(arg, "w") as outfile:
+            for line in infile:
+                outfile.write(line)
+        print("success")
+    except Exception as e:
+        print("failed")
+        print(e)
+if __name__ == "__main__":    
     if len(sys.argv) > 1:
         main(sys.argv[1])
     else:
-        print("No argument provided.")
+        print("failed")

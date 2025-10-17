05b37a406e849c66dfa7119f88bdf7f158ec1321
Suganya <572490@cognizant.com>
2025-10-17 10:15:25 +0530

    Fix

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 def main(arg):   
@@ -5,11 +6,17 @@ def main(arg):
         with open("testdata/genai-testdata.jsonl", "r") as infile, open(arg, "w") as outfile:
             for line in infile:
                 outfile.write(line)
-        print("success")
+        
+        result = "success"  # do your computation here
+        # Write a step output for GitHub Actions
+        with open(os.environ["GITHUB_OUTPUT"], "a") as f:
+            print(f"result={result}", file=f)
+
     except Exception as e:
         print("failed")
         print(e)
-if __name__ == "__main__":    
+if __name__ == "__main__":
+    print("hello world")
     if len(sys.argv) > 1:
         main(sys.argv[1])
     else:
